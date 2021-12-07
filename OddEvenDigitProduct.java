dfc8699a687ab912902187d6d33f68a89275fd5c
Psarkar777 <56498690+Psarkar777@users.noreply.github.com>
2021-12-07 12:50:19 +0530

    Add files via upload

--- OddEvenDigitProduct.java
+++ OddEvenDigitProduct.java
@@ -1,3 +1,38 @@
+/*
+Write an algorithm to find the discount
+
+amount for the given total bill amount.
+
+Input
+
+The input consists of an integer billAmount,
+
+representing the total bill amount of a
+
+customer.
+
+Output
+
+Print an integer representing the discount amount for the given total bill.
+
+Constraints
+
+0 < billAmounts 10
+
+Example
+
+Input: 2514795
+
+Output 162
+
+Explanation:
+
+Odd digits in the given number 2514795 are 5.1,7,9,5. The sum of these add digits is 27 Even digits in the given number 2514795 ace 2,4. The sum of these even digits &
+
+So, the output in 162
+*/
+
+
 package CompanyCoding;
 
 import java.util.Scanner;
